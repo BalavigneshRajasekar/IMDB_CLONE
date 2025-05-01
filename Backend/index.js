42a597a0f9cfbda1d061c0a6bfb0fdd571848d9f
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-05-01 20:38:18 +0530

    New : server connection , DB connection created

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+require("dotenv").config();
+const dbConnection = require("./dbConfig");
 const bodyParser = require("body-parser");
 
 const server = express();
+
+//Server start point
+
+server.listen("3000", () => {
+  console.log("server started");
+  dbConnection(process.env.MONGO_URI);
+});
